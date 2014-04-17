346a1bce6f026a2c5e1ff29f6fe90fc0f5d1b27e
artemiy619 <artem.sh2008@gmail.com>
2014-04-17 22:33:20 +0300

    Список контактов

---
+++ ListViewImages/src/com/jsupport/listviewimages/ItemListBaseAdapter.java
@@ -0,0 +1,69 @@
+package com.jsupport.listviewimages;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ItemListBaseAdapter extends BaseAdapter {
+	static class ViewHolder {
+		TextView txt_itemName;
+		TextView txt_itemDescription;
+		TextView txt_itemPrice;
+		ImageView itemImage;
+	}
+
+	private static ArrayList<ItemDetails> itemDetailsrrayList;
+
+	private LayoutInflater l_Inflater;
+
+	public ItemListBaseAdapter(Context context, ArrayList<ItemDetails> results) {
+		itemDetailsrrayList = results;
+		l_Inflater = LayoutInflater.from(context);
+	}
+
+	public int getCount() {
+		return itemDetailsrrayList.size();
+	}
+
+	public Object getItem(int position) {
+		return itemDetailsrrayList.get(position);
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		if (convertView == null) {
+			convertView = l_Inflater.inflate(R.layout.item_details_view, null);
+			holder = new ViewHolder();
+			holder.txt_itemName = (TextView) convertView
+					.findViewById(R.id.name);
+			holder.itemImage = (ImageView) convertView.findViewById(R.id.photo);
+
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+
+		holder.txt_itemName
+				.setText(itemDetailsrrayList.get(position).getName());
+		Bitmap b = itemDetailsrrayList.get(position).getBitmap();
+		if (b != null) {
+			holder.itemImage.setImageBitmap(b);
+		} else
+			holder.itemImage.setImageResource(R.drawable.noimage);
+
+		return convertView;
+	}
+
+	
+}
