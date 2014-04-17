7a3ada34c1f34871463deb2df843c719399ed948
JSmirnova <onaneodna@mail.ru>
2014-04-17 22:19:43 +0300

    локация
    показывает наше месторасположение и прокладывает маршрут в заданную
    точку.

---
+++ Location/src/com/example/location/MainActivity.java
@@ -0,0 +1,431 @@
+package com.example.location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.location.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+//import com.google.android.maps.GeoPoint;
+
+
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends FragmentActivity implements
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener,
+		com.google.android.gms.location.LocationListener {
+
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	// Milliseconds per second
+	private static final int MILLISECONDS_PER_SECOND = 1000;
+	// Update frequency in seconds
+	public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+	// Update frequency in milliseconds
+	private static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND
+			* UPDATE_INTERVAL_IN_SECONDS;
+	// The fastest update frequency, in seconds
+	private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+	// A fast frequency ceiling in milliseconds
+	private static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND
+			* FASTEST_INTERVAL_IN_SECONDS;
+
+	GoogleMap mMap;
+	LocationClient mLocationClient;
+	Location mCurrentLocation;
+	LocationRequest mLocationRequest;
+	boolean mUpdatesRequested;
+	SharedPreferences mPrefs;
+	Editor mEditor;
+	Marker marker;
+	Marker destinationMarker;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		/*
+		 * Create a new location client, using the enclosing class to handle
+		 * callbacks.
+		 */
+		mMap = ((SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
+		mLocationClient = new LocationClient(this, this, this);
+		mLocationRequest = LocationRequest.create();
+		// Use high accuracy
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		// Set the update interval to 5 seconds
+		mLocationRequest.setInterval(UPDATE_INTERVAL);
+		// Set the fastest update interval to 1 second
+		mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+		// Open the shared preferences
+		mPrefs = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
+		// Get a SharedPreferences editor
+		mEditor = mPrefs.edit();
+		// Start with updates turned off
+		mUpdatesRequested = false;
+
+	}
+
+	/*
+	 * Called when the Activity becomes visible.
+	 */
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Connect the client.
+		mLocationClient.connect();
+
+	}
+
+	/*
+	 * Called when the Activity is no longer visible.
+	 */
+	@Override
+	protected void onStop() {
+		// Disconnecting the client invalidates it.
+		// If the client is connected
+		if (mLocationClient.isConnected()) {
+			/*
+			 * Remove location updates for a listener. The current Activity is
+			 * the listener, so the argument is "this".
+			 */
+			mLocationClient.removeLocationUpdates(this);
+		}
+		/*
+		 * After disconnect() is called, the client is considered "dead".
+		 */
+		mLocationClient.disconnect();
+		super.onStop();
+	}
+
+	public void onLocationChanged(Location location) {
+		// Report to the UI that the location was updated
+
+		mCurrentLocation = location;
+		String msg = "Updated Location: "
+				+ Double.toString(location.getLatitude()) + ","
+				+ Double.toString(location.getLongitude());
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+		if (marker != null)
+			marker.remove();
+		Bitmap bmp = BitmapFactory.decodeResource(getResources(),
+				R.drawable.ic_launcher);
+//		marker = mMap.addMarker(new MarkerOptions()
+//				.position(
+//						new LatLng(mCurrentLocation.getLatitude(),
+//								mCurrentLocation.getLongitude()))
+//				.title("Julia Vovk").snippet("I'm here")
+//				.icon(BitmapDescriptorFactory.fromBitmap(bmp)));
+		mMap.setMyLocationEnabled(true);
+	}
+
+	/*
+	 * Called by Location Services when the request to connect the client
+	 * finishes successfully. At this point, you can request the current
+	 * location or start periodic updates
+	 */
+	@Override
+	public void onConnected(Bundle dataBundle) {
+		// Display the connection status
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		if (mUpdatesRequested) {
+			mLocationClient.requestLocationUpdates(mLocationRequest, this);
+		}
+
+		mCurrentLocation = mLocationClient.getLastLocation();
+		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+				new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation
+						.getLongitude()), 17));
+		mMap.setMyLocationEnabled(true);
+//		marker = mMap.addMarker(new MarkerOptions()
+//				.position(
+//						new LatLng(mCurrentLocation.getLatitude(),
+//								mCurrentLocation.getLongitude()))
+//				.title("Julia Vovk").snippet("I'm here"));
+		// mMap.addMarker(new MarkerOptions()
+		// .position(
+		// new LatLng(50.02,
+		// 36.2)));
+		mUpdatesRequested = true;
+
+		mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+			@Override
+			public void onMapClick(LatLng point) {
+				// TODO Auto-generated method stub
+				// lstLatLngs.add(point);
+
+				destinationMarker = mMap.addMarker(new MarkerOptions()
+						.position(point));
+				// onClickRoute(point);
+
+				// String url = makeURL(mCurrentLocation.getLatitude(),
+				// mCurrentLocation.getLongitude(),
+				// point.latitude, point.longitude);
+				// connectAsyncTask cat = new connectAsyncTask(url);
+				// cat.execute();
+			}
+		});
+
+	}
+
+	public void onClickRoute(View view) {
+		if (destinationMarker != null) {
+			String url = makeURL(mCurrentLocation.getLatitude(),
+					mCurrentLocation.getLongitude(),
+					destinationMarker.getPosition().latitude,
+					destinationMarker.getPosition().longitude);
+			connectAsyncTask cat = new connectAsyncTask(url);
+			cat.execute();
+		}
+		else {
+			AlertDialog.Builder b = new AlertDialog.Builder(this);
+			 b
+			    .setTitle("Error")
+			    .setMessage("Please, select place to create a route")
+			    .setIcon(android.R.drawable.ic_dialog_alert)
+			    .setPositiveButton("OK", new DialogInterface.OnClickListener() 
+			    {
+			        public void onClick(DialogInterface dialog, int which) 
+			        {       
+			               dialog.dismiss();
+			    }
+			    });             
+			    
+			    AlertDialog alert = b.create();
+			        alert.show();
+		}
+	}
+
+	protected void onPause() {
+		// Save the current setting for updates
+		mEditor.putBoolean("KEY_UPDATES_ON", mUpdatesRequested);
+		mEditor.commit();
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		/*
+		 * Get any previous setting for location updates Gets "false" if an
+		 * error occurs
+		 */
+		super.onResume();
+		if (mPrefs.contains("KEY_UPDATES_ON")) {
+			mUpdatesRequested = mPrefs.getBoolean("KEY_UPDATES_ON", false);
+
+			// Otherwise, turn off location updates
+		} else {
+			mEditor.putBoolean("KEY_UPDATES_ON", false);
+			mEditor.commit();
+		}
+	}
+
+	/*
+	 * Called by Location Services if the connection to the location client
+	 * drops because of an error.
+	 */
+	@Override
+	public void onDisconnected() {
+		// Display the connection status
+		Toast.makeText(this, "Disconnected. Please re-connect.",
+				Toast.LENGTH_SHORT).show();
+	}
+
+	/*
+	 * Called by Location Services if the attempt to Location Services fails.
+	 */
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		/*
+		 * Google Play services can resolve some errors it detects. If the error
+		 * has a resolution, try sending an Intent to start a Google Play
+		 * services activity that can resolve error.
+		 */
+		if (connectionResult.hasResolution()) {
+			try {
+				// Start an Activity that tries to resolve the error
+				connectionResult.startResolutionForResult(this,
+						CONNECTION_FAILURE_RESOLUTION_REQUEST);
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+			} catch (IntentSender.SendIntentException e) {
+				// Log the error
+				e.printStackTrace();
+			}
+		} else {
+			/*
+			 * If no resolution is available, display a dialog to the user with
+			 * the error.
+			 */
+
+			showDialog(connectionResult.getErrorCode());
+		}
+	}
+
+	public String makeURL(double sourcelat, double sourcelog, double destlat,
+			double destlog) {
+		StringBuilder urlString = new StringBuilder();
+		urlString.append("http://maps.googleapis.com/maps/api/directions/json");
+		urlString.append("?origin=");// from
+		urlString.append(Double.toString(sourcelat));
+		urlString.append(",");
+		urlString.append(Double.toString(sourcelog));
+		urlString.append("&destination=");// to
+		urlString.append(Double.toString(destlat));
+		urlString.append(",");
+		urlString.append(Double.toString(destlog));
+		urlString.append("&sensor=false&mode=driving&alternatives=true");
+		return urlString.toString();
+	}
+
+	public void drawPath(String result) {
+
+		try {
+			// Tranform the string into a json object
+			final JSONObject json = new JSONObject(result);
+			JSONArray routeArray = json.getJSONArray("routes");
+			JSONObject routes = routeArray.getJSONObject(0);
+			JSONObject overviewPolylines = routes
+					.getJSONObject("overview_polyline");
+			String encodedString = overviewPolylines.getString("points");
+			List<LatLng> list = decodePoly(encodedString);
+
+			for (int z = 0; z < list.size() - 1; z++) {
+				LatLng src = list.get(z);
+				LatLng dest = list.get(z + 1);
+				Polyline line = mMap.addPolyline(new PolylineOptions()
+						.add(new LatLng(src.latitude, src.longitude),
+								new LatLng(dest.latitude, dest.longitude))
+						.width(5).color(Color.RED).geodesic(true));
+			}
+
+		} catch (JSONException e) {
+			AlertDialog.Builder b = new AlertDialog.Builder(this);
+			 b
+			    .setTitle("Error")
+			    .setMessage("Fuck off, bitch!")
+			    .setIcon(android.R.drawable.ic_dialog_alert)
+			    .setPositiveButton("OKAY=(", new DialogInterface.OnClickListener() 
+			    {
+			        public void onClick(DialogInterface dialog, int which) 
+			        {       
+			               dialog.dismiss();
+			    }
+			    });             
+			    
+			    AlertDialog alert = b.create();
+			        alert.show();
+		}
+	}
+
+	private List<LatLng> decodePoly(String encoded) {
+
+		List<LatLng> poly = new ArrayList<LatLng>();
+		int index = 0, len = encoded.length();
+		int lat = 0, lng = 0;
+
+		while (index < len) {
+			int b, shift = 0, result = 0;
+			do {
+				b = encoded.charAt(index++) - 63;
+				result |= (b & 0x1f) << shift;
+				shift += 5;
+			} while (b >= 0x20);
+			int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+			lat += dlat;
+
+			shift = 0;
+			result = 0;
+			do {
+				b = encoded.charAt(index++) - 63;
+				result |= (b & 0x1f) << shift;
+				shift += 5;
+			} while (b >= 0x20);
+			int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+			lng += dlng;
+
+			LatLng p = new LatLng((((double) lat / 1E5)),
+					(((double) lng / 1E5)));
+			poly.add(p);
+		}
+
+		return poly;
+	}
+
+	private class connectAsyncTask extends AsyncTask<Void, Void, String> {
+		private ProgressDialog progressDialog;
+		String url;
+
+		public connectAsyncTask(String urlPass) {
+			url = urlPass;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			progressDialog = new ProgressDialog(MainActivity.this);
+			progressDialog.setMessage("Fetching route, Please wait...");
+			progressDialog.setIndeterminate(true);
+			progressDialog.show();
+		}
+
+		@Override
+		protected String doInBackground(Void... params) {
+			JSONParser jParser = new JSONParser();
+			String json = jParser.getJSONFromUrl(url);
+			return json;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			progressDialog.hide();
+			if (result != null) {
+				drawPath(result);
+			}
+		}
+	}
+}
